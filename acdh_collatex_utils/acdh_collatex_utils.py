2302ed0e3db0388b8e629b845b61579554eebcd2
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2020-12-11 10:56:36 +0100

    function and method for chunking texts added

--- acdh_collatex_utils/acdh_collatex_utils.py
+++ acdh_collatex_utils/acdh_collatex_utils.py
@@ -2,12 +2,15 @@ import os
 
 import lxml.etree as ET
 from . xml import XMLReader
+from . chunks import get_chunks
 
 CUSTOM_XSL = os.path.join(
     os.path.dirname(__file__),
     "xslt",
     "only_text.xsl"
 )
+
+CHUNK_SIZE = 2000
 
 
 class CxReader(XMLReader):
@@ -40,10 +43,20 @@ class CxReader(XMLReader):
             return plain_text[:5000]
         return plain_text
 
+    def yield_chunks(self):
+
+        """ yields chunks of the object's plain text
+
+        :return:  yields dicts like `{"id": "example_text.txt", "chunk_nr": "001", "text": "text of chunk", "char_count": 13}`
+        :rtype: dict
+        """
+        return get_chunks(self.plain_text, self.chunk_size, self.file_name)
+
     def __init__(
         self,
         custom_xsl=CUSTOM_XSL,
         char_limit=True,
+        chunk_size=CHUNK_SIZE,
         **kwargs
     ):
 
@@ -55,6 +68,9 @@ class CxReader(XMLReader):
         :param char_limit: Should the number of chars of the plaintext be limited?
         :type custom_xsl: bool
 
+        :param chunk_size: Size of chunks yield by `yield_chunks`
+        :type custom_xsl: int
+
         :return: A CxReader instance
         :rtype: `acdh_collatex_utils.CxReader`
 
@@ -62,7 +78,12 @@ class CxReader(XMLReader):
         super().__init__(**kwargs)
         self.custom_xsl = ET.parse(custom_xsl)
         self.char_limit = char_limit
+        self.chunk_size = chunk_size
         self.cur_doc = self.preprocess()
         self.plain_text = self.plain_text()
         self.collatex_wit = (self.file, self.plain_text)
         self.plaint_text_len = len(self.plain_text)
+        try:
+            self.file_name = os.path.split(self.file)[1]
+        except Exception as e:
+            self.file_name = self.file
