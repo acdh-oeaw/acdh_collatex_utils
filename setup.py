fabf8114b179721dff0035c309aa214ca6f1f85c
csae8092 <peter.andorfer@oeaw.ac.at>
2020-12-17 12:55:45 +0100

    Bump version: 1.0.2 → 1.1.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acdh-oeaw/acdh_collatex_utils',
-    version='1.0.2',
+    version='1.1.0',
     zip_safe=False,
 )
